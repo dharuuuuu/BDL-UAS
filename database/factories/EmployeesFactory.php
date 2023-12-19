f2528df84d3ae9401680b2ed7c16ca53bd5429b4
Dimas Dharu Ramadhan <dimasddr7@gmail.com>
2023-12-19 14:55:58 +0700

    change seed name indonesian

--- database/factories/EmployeesFactory.php
+++ database/factories/EmployeesFactory.php
@@ -5,8 +5,7 @@ namespace Database\Factories;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
-
+use Faker\Provider\id_ID\Person;
 
 class EmployeesFactory extends Factory
 {
@@ -17,12 +16,15 @@ class EmployeesFactory extends Factory
         // Round the random number to the nearest million
         $roundedNumber = round($randomNumber * 1000000);
 
+        // Set the Faker locale to Indonesian
+        $this->faker->addProvider(new Person($this->faker));
+
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'join_date' => $this->faker->dateTimeBetween('-1 years', 'now'),
             'salary' => $roundedNumber,
-            'age' => rand(20,50),
+            'age' => rand(20, 50),
             'gender' => $this->faker->randomElement(['Male', 'Female']),
             'position' => $this->faker->randomElement(['Junior Web', 'Senior Web', 'Designer']),
         ];
